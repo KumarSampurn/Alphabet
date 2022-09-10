2387da78780b2c07935ebd9078e08b4d7edfd022
Epsilon <101011193+KumarSampurn@users.noreply.github.com>
2022-09-10 16:38:54 +0530

    Added Cross hair in the middle

--- capture.py
+++ capture.py
@@ -1,49 +1,53 @@
-import cv2 
+import cv2
 
-key = cv2. waitKey(1)
-webcam = cv2.VideoCapture(0)
+cap=cv2.VideoCapture(0)
+
+output= cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*'MPEG'),30,(1080,1920))
+
 while True:
     try:
-        check, frame = webcam.read()
-        
-        cv2.imshow("Capturing", frame)
-        key = cv2.waitKey(1)
-        if key == ord('s'): 
+        ret, frame= cap.read()
+        if(ret):
+            # rectange
+            cv2.rectangle(frame, (225,124),(425,325),(0.255,0))
+            #verticle line
+            cv2.rectangle(frame, (325, 175),(325,275),(0,255,0))
+            #horizontal line
+            cv2.rectangle(frame, (275, 225), (375, 225),(0, 255, 0))    
 
+            output.write(frame)
+            cv2.imshow("output",frame)
+            key = cv2.waitKey(1)
+        if key== ord('s'):
             cv2.imwrite(filename='captured_image_color.jpg', img=frame)
-            webcam.release()
+            cap.release()
             img_new = cv2.imread('captured_image_color.jpg', cv2.IMREAD_GRAYSCALE)
             img_new = cv2.imshow("Captured Image", img_new)
             cv2.waitKey(1650)
             cv2.destroyAllWindows()
-            
 
             img_ = cv2.imread('captured_image_color.jpg', cv2.IMREAD_ANYCOLOR)
 
-            print("Converting RGB image to grayscale...")
             gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-          
-            print("Resizing image to 500x500 scale...")
+
             img_ = cv2.resize(gray,(500,500))
 
-           
             img_resized = cv2.imwrite(filename='captured_image_greyscale-final.jpg', img=img_)
             print("Image saved!")
-        
+
             break
         elif key == ord('q'):
             print("Turning off camera.")
-            webcam.release()
+            cap.release()
             print("Camera off.")
             print("Program ended.")
             cv2.destroyAllWindows()
             break
-        
+
     except(KeyboardInterrupt):
         print("Turning off camera.")
-        webcam.release()
+        cap.release()
         print("Camera off.")
         print("Program ended.")
         cv2.destroyAllWindows()
         break
-    
